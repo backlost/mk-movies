ae840d3ca701d22a05acfe6dc6ee93505329253c
meng.xianliang <meng.xianliang@immomo.com>
2016-10-09 18:16:07 +0800

    fix some bugs

---
+++ index.js
@@ -0,0 +1,22 @@
+import Icon from './components/base/icon';
+import Mask from './components/base/mask';
+import Button from './components/normal/button';
+import ListItem from './components/normal/list-item';
+import Alert from './components/popup/alert';
+import Confirm from './components/popup/confirm';
+import Loading from './components/popup/loading';
+import ShowTip from './components/popup/show-tip';
+
+
+const movies = {
+    Icon,
+	Mask,
+	Button,
+    ListItem,
+    Alert,
+    Confirm,
+    Loading,
+    ShowTip
+};
+
+module.exports = movies;
